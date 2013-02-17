2dfc757fec00c19a2ab70f0fd4b4d76bd82b6a11
Marcio Arend <marcioarend@gmail.com>
2013-02-17 13:19:31 +0100

    primeira vez

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ontrack::Application.config.secret_token = 'ee32ca834692fd7dba0f71dc300cf40ba8bdb71c2f74c90600d25fb1beaea3a000d4070b19fd0184e8bb01bef6d622ca71c53d7bea29421bca10ecfde8e9d325'
